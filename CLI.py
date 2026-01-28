6f5bbd4aead7bce7235d0b3b57e6c336a5707d88
DIvyesh <divyeshgaigol16@gmail.com>
2026-01-28 23:51:09 +0530

    minor change in CLI.py

--- CLI.py
+++ CLI.py
@@ -1,10 +1,10 @@
 import sys
 import os
-
+from containcraft.cli import main
 # Add current directory to path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from containcraft.cli import main
+
 
 if __name__ == "__main__":
     main()
